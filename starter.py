889f0e89c97bffee61597a417d36836d0eaf6285
Nichole Boothman <nbooth731@gmail.com>
2021-12-09 15:24:29 -0500

    Complete

--- starter.py
+++ starter.py
@@ -47,7 +47,7 @@ for number in numbers:
 even_numbers = []
 for number in even_numbers:
     if number % 2 == 0:
-        even_numbers.push(number)
+        even_numbers.append(number)
 print(even_numbers)
 
 # Problem 10
@@ -80,19 +80,24 @@ elif change_my_mind == False:
 
 # For problems 12-15, use the following line of code:
 friends = ['Joe', 'Sally', 'Camilo', 'Perry', 'Susan']
+print(friends)
 
 # Problem 12
 # Research to find the Python method that allows you to add an element to the end of the array (similar to push() in JavaScript), then add a name to the end of the `friends` array.
 friends.append('Nicki')
+print(friends)
 
 # Problem 13
 # Print out the total amount of elements in the `friends` array. The Python method you are looking for is similar to the JavaScript property `.length`.
 num_of_friends = len(friends)
+print(num_of_friends)
 
 # Problem 14
 # Add a name into the third position in the array (index 2). Make sure you are not overwriting the value that is already there.
 friends.insert(friends[2], 'Frank')
+print(friends)
 
 # Problem 15
 # Remove the last item in the array (try to think about how you can do this dynamically, meaning, if the array contents were to change, your code would still work).
 friends.pop()
+print(friends)
